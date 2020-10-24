3440997231c1121de324d2ae6a4d83cd759fc6b6
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2020-10-24 11:58:34 +0300

    indentation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -8,17 +8,13 @@ export function findErrors(values ,emailControl ,passControl) {
     const localValidationSchema = {
 
         ...validationSchema ,
-
         email:validationSchema.email.filter(rule => emailControl[rule.id]),
         password:validationSchema.password.filter(rule => passControl[rule.id])
-
     }
-
 
     Object
         .entries(values)
-        .forEach(([key, value]) => {
-
+            .forEach(([key, value]) => {
 
             if(localValidationSchema[key].some(rule => !rule.validator(value))){
 
@@ -27,7 +23,6 @@ export function findErrors(values ,emailControl ,passControl) {
                 errors[key] = localValidationSchema[key].filter(rule => !rule.validator(value))
                 .map(error => error.message); 
             }            
-
         }
 
         );
